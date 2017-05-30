0df991408dfd76c412e7f114e0e295a83e440333
carolinethomson <cethomson2@gmail.com>
2017-05-30 15:31:22 +0200

    Add colour palettes and function

---
+++ colourlists.R
@@ -0,0 +1,14 @@
+ghibli_palettes<-list(
+CastleintheSky=c("#5f8bcc","#3069c2","#2f4337","#4e693c","#77a66f","#8f3535","#a7784e","#985d32","#e2d234","#d9db6e"),
+
+KikiDeliveryService=c("#3f97a3","#2188a7","#1d3e6b","#19479d","#8e9753","#d4d770","#3f406e","#cab9b2","#2b0908","#c53840"),
+
+TheWindRises=c("#f0c4a7","#f1c8aa","#cfacb3","#f8e5ad","#16a13a","#ece3d4","#1baa41","#007db3","#008fbb","#0096c3"),
+
+Totoro=c("#876c57","#473535","#ece3d4","#82c1ed","#f38524","#fcf95e","#fefc8d","#5096eb","#fefec2","#567442"),
+
+PrincessMononoke=c("#171916","#473535","#a79297","#6a5157","#8898b1","#efefef","#4c5119","#eebca1","#e07d6a","#bc3d36"),
+
+Nausicaa=c("#002627","#556a59","#6e6046","#deacab","#f5f1e6","#ffea81","#fbe5b3","#dce4cf","#36b3df","#0598ce"),
+
+Ponyo=c("#dcedff","#25387c","#37458e","#304795","#5284c3","#62a0dd","#80b5df","#651025","#994264","#b03d36"))
---
+++ function.R
@@ -0,0 +1,36 @@
+ghibli_palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+  
+  pal <- ghibli_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+  
+  if (missing(n)) {
+    n <- length(pal)
+  }
+  
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+  
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal)(n),
+                discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
+}
+
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+  
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+  
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+}
